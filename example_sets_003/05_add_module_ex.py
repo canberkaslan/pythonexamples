6500f8a669a77670673967d8af6462c1ac25af8f
canberkaslan <canberkaslan@gmail.com>
2019-11-21 15:22:04 +0300

    example

---
+++ example_sets_003/05_add_module_ex.py
@@ -0,0 +1,5 @@
+import mod
+
+result = mod.IPs
+
+print(result)
---
+++ example_sets_003/mod.py
@@ -0,0 +1,23 @@
+'''
+comment area
+'''
+
+print("Modul ekrana basma")
+
+number=4
+numbers =[1,6,7,5]
+
+IPs = ['123.323.232.433','253.323.232.433']
+
+user = {
+    "firstname":'canberk',
+    "lastname":'aslan',
+    "age":"31"
+}
+
+def func01(x):
+    '''
+        comment area
+
+    '''
+    print(x)
