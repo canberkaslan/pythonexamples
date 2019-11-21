f8acb6c8ac981d20c6507b0728dfa8be9227a7dd
canberkaslan <canberkaslan@gmail.com>
2019-11-21 15:13:43 +0300

    example

--- example_sets_003/04_random.py
+++ example_sets_003/04_random.py
@@ -2,3 +2,10 @@ import random
 names = ['ali','canberk','ahmet','veli','hakan']
 result = names[random.randint(0,len(names)-1)]
 print(result)
+
+
+liste = list(range(10))
+print(liste)
+random.shuffle(liste)
+print(liste)
+
