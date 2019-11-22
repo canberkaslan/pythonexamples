0298551ec48a4c820e35d8e98d654436be6e9fc6
canberkaslan <canberkaslan@gmail.com>
2019-11-22 15:15:17 +0300

    file processes

---
+++ example_sets_004/09_sqlite3.py
@@ -0,0 +1,16 @@
+# Android veya IOS sistemlerde sqlite gelir bu dunyada kullanılan opensource bır db
+
+import sqlite3
+
+
+connection = sqlite3.connect("chinook.db")
+
+cursor = connection.cursor()
+cursor.execute("select * from customers")
+result= cursor.fetchall()
+
+
+for i in result:
+    print(i)
+
+connection.close()
