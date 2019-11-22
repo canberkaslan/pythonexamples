2040036afa1e645d69117d8812249487b0cd21e2
canberkaslan <canberkaslan@gmail.com>
2019-11-22 14:47:55 +0300

    file processes

--- example_sets_004/06_request.py
+++ example_sets_004/06_request.py
@@ -1,2 +1,30 @@
 import json
 import requests
+
+
+#Example 1
+#result =requests.get("https://jsonplaceholder.typicode.com/todos")
+#result = json.loads(result.text)
+#for  x in result:
+#    if x["userId"] == 1:
+#        print(x["title"])
+#
+#print(type(result))
+
+#Example 2
+api_url = "https://api.exchangeratesapi.io/latest?base="
+bozulanDoviz = input("Döviz Türü (Bozulan) : ")
+alinanDoviz = input("Döviz Türü (Alinan) : ")
+miktar = int(input(f"Ne kadar {bozulanDoviz} bozdurmak istiyorsunuz ? : "))
+
+result = requests.get(api_url + bozulanDoviz)
+result = json.loads(result.text)
+
+print("1 {0} = {1} {2}".format(bozulanDoviz,
+                                result["rates"][alinanDoviz],
+                                alinanDoviz))
+
+print("{0} {1} = {2} {3}".format(miktar,
+                                bozulanDoviz,
+                                miktar*result["rates"][alinanDoviz],
+                                alinanDoviz))
